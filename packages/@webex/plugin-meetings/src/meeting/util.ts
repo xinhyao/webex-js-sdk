425cdb682ebfbf860d63a6630f91e7b39cb52c99
Marcin <marcin.bazyl@gmail.com>
2024-12-04 16:49:13 +0000

    feat(meetings): incremental log uploads (#3899)

--- packages/@webex/plugin-meetings/src/meeting/util.ts
+++ packages/@webex/plugin-meetings/src/meeting/util.ts
@@ -197,6 +197,7 @@ const MeetingUtil = {
 
   cleanUp: (meeting) => {
     meeting.getWebexObject().internal.device.meetingEnded();
+    meeting.stopPeriodicLogUpload();
 
     meeting.breakouts.cleanUp();
     meeting.simultaneousInterpretation.cleanUp();
